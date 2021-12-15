5d0c557c724830481392d94376f7f54be820dad7
djambred <djambred@gmail.com>
2021-12-16 01:30:47 +0700

    fresh from oven

---
+++ pert11/fun.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void fname(){
+    cout << "Jefry";
+}
+
+void mname(){
+    cout << "Sunupurwa";
+}
+
+void lname(){
+    cout << "Asri";
+}
+
+void spasi(){
+    cout<<" ";
+}
+
+int main() {
+    fname();
+    spasi();
+    mname();
+    spasi();
+    lname();
+    return 0;
+}
